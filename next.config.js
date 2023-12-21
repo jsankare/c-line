1d102fedb187ac524ffde6681bdbc13e4f622a95
jordan sankare <jordan.sankare@gmail.com>
2023-12-21 13:51:26 +0100

    change next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  output: 'export',
+  images: {
+      unoptimized : true
+  },
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  }
 }
-
-module.exports = nextConfig
